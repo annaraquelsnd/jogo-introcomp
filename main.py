55731925c921f2d90e28d65f5d011d7b7fe1ddbe
annaraquelsnd <annaraquelsandrini@gmail.com>
2025-09-27 09:13:31 -0300

    Criando repositório

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+largura = 1024
+altura = 768
+
+janela = pygame.display.set_mode((largura, altura))
+
+def jogo():
+    
+    while(True):
+        #Clock
+        clock.tick(fps)
+
+        #Input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        #Atualizacao
+
+        #Renderizacao
+        janela.fill("white")
+        pygame.display.flip()
+
+jogo()
